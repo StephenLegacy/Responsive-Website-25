c812e6124f177051fea750144e50dd611a858977
StephenLegacy <oloostephen20191@gmail.com>
2025-04-30 11:32:00 +0300

    Add new changes

---
+++ ideal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# DATE
+DATE="2025-04-30T11:32:00"
+
+# Add and commit each file separately
+git add .
+GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "Add new changes"
+
+git push origin main
